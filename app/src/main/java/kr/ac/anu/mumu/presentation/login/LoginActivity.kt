7ed9f6cc052adec17817f383f720f34b20a8ee24
bhindor <bhindor@dgsw.hs.kr>
2026-02-05 19:55:41 +0900

    ⚡️ :: add login file
    ⚡️ :: binding setting

---
+++ app/src/main/java/kr/ac/anu/mumu/presentation/login/LoginActivity.kt
@@ -0,0 +1,26 @@
+package kr.ac.anu.mumu.presentation.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kr.ac.anu.mumu.R
+import kr.ac.anu.mumu.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityLoginBinding.inflate(layoutInflater) 
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
