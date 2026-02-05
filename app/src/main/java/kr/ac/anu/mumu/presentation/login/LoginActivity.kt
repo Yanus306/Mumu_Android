3d16dd16e95954f922209508c282ec81aca31a69
bhindor <bhindor@dgsw.hs.kr>
2026-02-05 22:16:52 +0900

    ⚡️ :: main title view

--- app/src/main/java/kr/ac/anu/mumu/presentation/login/LoginActivity.kt
+++ app/src/main/java/kr/ac/anu/mumu/presentation/login/LoginActivity.kt
@@ -1,8 +1,12 @@
 package kr.ac.anu.mumu.presentation.login
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kr.ac.anu.mumu.R
@@ -22,5 +26,29 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        viewSet()
+    }
+
+    private fun viewSet() {
+        val originText = binding.tvTitle.text.toString()
+        val spannable = SpannableStringBuilder(originText)
+
+        val targetWord = "Mumu"
+        val start = originText.indexOf(targetWord)
+        val end = start + targetWord.length
+
+        if (start != -1) {
+            val color = ContextCompat.getColor(this, R.color.mumumint_300)
+
+            spannable.setSpan(
+                ForegroundColorSpan(color),
+                start,
+                end,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+
+        binding.tvTitle.text = spannable
     }
 }
