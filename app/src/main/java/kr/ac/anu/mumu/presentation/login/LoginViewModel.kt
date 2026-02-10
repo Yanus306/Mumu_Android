ce4665e7c74c5107da871d599bc14e4a476d417a
bhindor <bhindor@dgsw.hs.kr>
2026-02-10 21:11:23 +0900

    ⚡️ :: LoginViewModel

--- app/src/main/java/kr/ac/anu/mumu/presentation/login/LoginViewModel.kt
+++ app/src/main/java/kr/ac/anu/mumu/presentation/login/LoginViewModel.kt
@@ -1,10 +1,44 @@
 package kr.ac.anu.mumu.presentation.login
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kr.ac.anu.mumu.domain.usecase.LoginUseCase
+import javax.inject.Inject
 
-class LoginViewModel : ViewModel() {
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val loginUseCase: LoginUseCase
+) : ViewModel() {
 
-    fun do_login(id: String, pw: String) {
+    // 화면 상태
+    sealed class UiState {
+        object Idle : UiState() // 대기
+        object Loading : UiState() // 로딩
+        object Success : UiState() // 로그인 성공
+        data class Error(val message: String) : UiState() // 실패
+    }
 
+    private val _loginState = MutableStateFlow<UiState>(UiState.Idle)
+    val loginState: StateFlow<UiState> = _loginState.asStateFlow()
+
+    // 로그인 함수
+    fun login(id: String, pw: String) {
+        viewModelScope.launch {
+            _loginState.value = UiState.Loading
+
+            loginUseCase(id, pw)
+                .onSuccess {
+                    _loginState.value = UiState.Success
+                }
+                .onFailure { error ->
+                    _loginState.value = UiState.Error(error.message ?: "로그인 실패")
+                }
+
+        }
     }
 }
