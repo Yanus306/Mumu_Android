ce7b8d27623cf9799624f5a9fb503aa389ba99dd
bhindor <bhindor@dgsw.hs.kr>
2026-02-10 16:43:35 +0900

    ⚡️ :: UseCase

--- app/src/main/java/kr/ac/anu/mumu/domain/usecase/LoginUseCase.kt
+++ app/src/main/java/kr/ac/anu/mumu/domain/usecase/LoginUseCase.kt
@@ -1,4 +1,13 @@
 package kr.ac.anu.mumu.domain.usecase
 
-class LoginUseCase @Inject constructor() {
+import kr.ac.anu.mumu.domain.model.User
+import kr.ac.anu.mumu.domain.repository.LoginRepository
+import javax.inject.Inject
+
+class LoginUseCase @Inject constructor(
+    private val repository: LoginRepository
+) {
+    suspend operator fun invoke(id: String, pw: String): Result<User> {
+        return repository.login(id, pw)
+    }
 }
