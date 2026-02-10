162e8a8fcb2ba808499c3f1efff66e20da7b34cb
bhindor <bhindor@dgsw.hs.kr>
2026-02-10 19:56:48 +0900

    ⚡️ :: RepositoryModule
    인터페이스와 구현체 연결

---
+++ app/src/main/java/kr/ac/anu/mumu/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package kr.ac.anu.mumu.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kr.ac.anu.mumu.data.repository.LoginRepositoryImpl
+import kr.ac.anu.mumu.domain.repository.LoginRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindLoginRepository(
+        loginRepositoryImpl: LoginRepositoryImpl
+    ): LoginRepository
+}
