1b03aff59609c5799e5715111316e9689936df73
bhindor <bhindor@dgsw.hs.kr>
2026-02-11 23:50:28 +0900

    ⚡️ :: add NameFragment

---
+++ app/src/main/java/kr/ac/anu/mumu/presentation/join/NameFragment.kt
@@ -0,0 +1,11 @@
+package kr.ac.anu.mumu.presentation.join
+
+import androidx.fragment.app.Fragment
+import kr.ac.anu.mumu.databinding.FragmentNameBinding
+
+class NameFragment : Fragment() {
+    private var _binding: FragmentNameBinding? = null
+    private val binding get() = _binding!!
+
+
+}
