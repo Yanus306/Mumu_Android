4b26b43c55c773f185169df5315f67352afa8eb4
bhindor <20250199@student.anu.ac.kr>
2026-02-23 06:55:51 +0900

    ⚡️ :: 주소 fragment 추가

---
+++ app/src/main/java/kr/ac/anu/mumu/presentation/join/AddressFragment.kt
@@ -0,0 +1,34 @@
+package kr.ac.anu.mumu.presentation.join
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import kr.ac.anu.mumu.databinding.FragmentAddressBinding
+
+class AddressFragment : Fragment() {
+    private var _binding: FragmentAddressBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: JoinViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAddressBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
