e913b6ddd68a42627525987d349be43da9c7fa6c
bhindor <20250199@student.anu.ac.kr>
2026-02-23 15:10:37 +0900

    ⚡️ :: 주소 뷰 수정

--- app/src/main/java/kr/ac/anu/mumu/presentation/join/AddressFragment.kt
+++ app/src/main/java/kr/ac/anu/mumu/presentation/join/AddressFragment.kt
@@ -1,12 +1,17 @@
 package kr.ac.anu.mumu.presentation.join
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import kr.ac.anu.mumu.R
 import kr.ac.anu.mumu.databinding.FragmentAddressBinding
 
 class AddressFragment : Fragment() {
@@ -25,6 +30,8 @@ class AddressFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewSet()
 
         viewModel.checkAddressStep()
 
@@ -47,6 +54,28 @@ class AddressFragment : Fragment() {
 
             viewModel.checkAddressStep()
         }
+    }
+
+    private fun viewSet() {
+        val originText = binding.tvTitle.text.toString()
+        val spannable = SpannableStringBuilder(originText)
+
+        val targetWord = "Mumu"
+        val start = originText.indexOf(targetWord)
+        val end = start + targetWord.length
+
+        if (start != -1) {
+            val color = ContextCompat.getColor(requireContext(), R.color.mumumint_300)
+
+            spannable.setSpan(
+                ForegroundColorSpan(color),
+                start,
+                end,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+
+        binding.tvTitle.text = spannable
     }
 
     override fun onDestroyView() {
