c14440b7f126d772741a027da5233cd63990e36f
Seung-netizen <juioman76@gmail.com>
2026-07-29 18:00:17 +0900

    feat: 마이 화면 구성
    하단 네비게이션 바(x)

---
+++ app/src/main/java/kr/ac/anu/mumu/presentation/my/MyActivity.kt
@@ -0,0 +1,32 @@
+package kr.ac.anu.mumu.presentation.my
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kr.ac.anu.mumu.R
+import kr.ac.anu.mumu.databinding.ActivityMyBinding
+
+class MyActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMyBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    private fun nextScreen(){
+        binding.layoutModifyInformation.setOnClickListener {
+            val intent = Intent(this, )
+        }
+    }
+}
