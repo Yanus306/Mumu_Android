fa38b7eb5706efe7b5ac65b6f526b9444d7705a5
Seung-netizen <juioman76@gmail.com>
2026-07-30 14:56:54 +0900

    feat: 마이_정보수정
    하단 네비게이션 바(x)

--- app/src/main/java/kr/ac/anu/mumu/presentation/my/ModifyInformationActivity.kt
+++ app/src/main/java/kr/ac/anu/mumu/presentation/my/ModifyInformationActivity.kt
@@ -1,34 +1,22 @@
 package kr.ac.anu.mumu.presentation.my
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kr.ac.anu.mumu.R
-import kr.ac.anu.mumu.databinding.ActivityMyBinding
-import kotlin.jvm.java
 
-class MyActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMyBinding
+class ModifyInformationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMyBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_modify_information)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-    }
-    private fun nextScreen(){
-        binding.layoutModifyInformation.setOnClickListener {
-            val intent = Intent(this, ModifyInformationActivity::class.java)
-            startActivity(intent)
         }
     }
 }
