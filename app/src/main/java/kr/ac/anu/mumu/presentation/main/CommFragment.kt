62e8858dde3234c71a1e5edb8cd6ed7c5201f5ac
bhindor <82446373+bhindor@users.noreply.github.com>
2026-08-26 14:24:38 +0900

    ⚡️ :: add community

---
+++ app/src/main/java/kr/ac/anu/mumu/presentation/main/CommFragment.kt
@@ -0,0 +1,31 @@
+package kr.ac.anu.mumu.presentation.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import kr.ac.anu.mumu.databinding.FragmentCommBinding
+import kr.ac.anu.mumu.presentation.join.JoinViewModel
+
+class CommFragment : Fragment() {
+    private var _binding: FragmentCommBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: JoinViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCommBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
